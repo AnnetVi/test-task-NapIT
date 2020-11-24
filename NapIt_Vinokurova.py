885b932e3d7857357d16dbf5c889a1be7a86fa84
AnnetVi <74953737+AnnetVi@users.noreply.github.com>
2020-11-24 12:41:12 +0300

    Update NapIt_Vinokurova.py
    Изменение отступов

--- NapIt_Vinokurova.py
+++ NapIt_Vinokurova.py
@@ -1,16 +1,16 @@
 class Solution:
-    def romanToInt(self,s: str) -> int:        
-          roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-          result = 0
-          try:
-            for i in range(len(s)):
-              if i>0 and roman[s[i]] > roman[s[i-1]] :
-                result += roman[s[i]] - 2*roman[s[i-1]]
-              else:
-                result += roman[s[i]]
-          except KeyError:
-            return -1
-          return result if 1 <= result <=3999 else 0
+  def romanToInt(self,s: str) -> int:        
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    try:
+      for i in range(len(s)):
+        if i>0 and roman[s[i]] > roman[s[i-1]] :
+          result += roman[s[i]] - 2*roman[s[i-1]]
+        else:
+          result += roman[s[i]]
+    except KeyError:
+      return -1
+    return result if 1 <= result <=3999 else 0
 if __name__ == "__main__":
   s=input("s = ")
   if 1<=len(s)<=15:
